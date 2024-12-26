7686a9b5dea40255f1d7eab267fcd58b077288de
sajid <sajidbrong@gmail.com>
2024-12-26 14:30:59 -0500

    handle revers geocode

--- src/views/BinPage.vue
+++ src/views/BinPage.vue
@@ -31,9 +31,9 @@
 
             <template #status="{ row }">
                 <div :class="{
-                    'bg-green-100 text-green-700': row.status === 'Empty',
-                    'bg-yellow-100 text-yellow-700': row.status === 'In Progress',
-                    'bg-red-100 text-red-700': row.status === 'Full',
+                    'bg-green-100 text-green-700': row.status === 'EMPTY',
+                    // 'bg-yellow-100 text-yellow-700': row.status === 'In Progress',
+                    'bg-red-100 text-red-700': row.status === 'FULL',
                 }"
                     class="inline-flex items-center justify-center px-3 py-1 text-sm font-medium rounded-full shadow-sm">
                     {{ row.status }}
